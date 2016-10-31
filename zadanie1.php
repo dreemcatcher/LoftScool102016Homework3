eab67a6ad0f3c45567d72da1f00cf0db639a70fd
Anton Vinogradov <etheralgreen@gmail.com>
2016-10-31 20:02:15 +1100

    #1[1] Задача 1. Вывод данных из XML.

--- zadanie1.php
+++ zadanie1.php
@@ -1,8 +1,31 @@
 <?php
-error_reporting(-1);
-mb_internal_encoding('utf-8');
-function myReadFromFile($fileName){
-    $result=file_get_contents($fileName);
-    return $result;
+$xml = simplexml_load_file("data.xml");
+
+echo 'Номер заказа - '.$xml[PurchaseOrderNumber];
+echo '<br/>';
+echo 'Дата заказа - '.$xml[OrderDate];
+echo '<br/>';
+
+echo '<pre>';
+echo '</pre>';
+echo '<br/>';
+echo 'Имя заказчика - '.$xml->Address[0]->Name;
+echo '<br/>';
+echo 'Улица\дом - '.$xml->Address[0]->Street;
+echo '<br/>';
+echo 'Город - '.$xml->Address[0]->City;
+echo '<br/>';
+echo 'Штат - '.$xml->Address[0]->State;
+echo '<br/>';
+echo 'Пометки - '.$xml->DeliveryNotes[0];
+echo '<br/>';
+
+foreach ($xml->Items->Item as $item) {
+    echo '<br/>';
+    echo '----------------------------------<br/>';
+    echo 'Номер партии '.$item[PartNumber]. '<br/>';
+    echo 'Наименование '.$item->ProductName. '<br/>';
+    echo 'Количество '.$item->Quantity. '<br/>';
+    echo 'Цена '.$item->USPrice. '<br/>';
+    echo 'Дата доставки '.$item->ShipDate. '<br/>';
 }
-echo myReadFromFile('data.xml');
