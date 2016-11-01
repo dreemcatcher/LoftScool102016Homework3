cefe61cd813d91cbbecc9a6ee57e228cadd96cc4
Anton Vinogradov <etheralgreen@gmail.com>
2016-11-01 17:56:54 +1100

    #3[1] Задача 3. Работа с CSV.

---
+++ zadanie4.php
@@ -0,0 +1,20 @@
+<?php
+
+function file_get_contents_curl($url) {
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); //Устанавливаем параметр, чтобы curl возвращал данные, вместо того, чтобы выводить их в браузер.
+    curl_setopt($ch, CURLOPT_URL, $url);
+
+    $data = curl_exec($ch);
+
+    echo "<pre>";
+    print_r($data);
+    curl_close($ch);
+}
+
+$content=file_get_contents_curl("https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json");
+
+echo $content;
+
