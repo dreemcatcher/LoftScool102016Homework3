113e63fd7160e564a998071829ad8b64c57f6555
Anton Vinogradov <etheralgreen@gmail.com>
2016-11-05 23:58:51 +1100

    new code comments for Task2,3,4

--- zadanie3.php
+++ zadanie3.php
@@ -3,32 +3,32 @@ error_reporting(-1);
 mb_internal_encoding('utf-8');
 
 function createMyCsv(){
-    $fileName="test.csv";
-    $maxArray=mt_rand(50,100);
-    $csvArray=[];
-    for ($i=0;$i<$maxArray;$i++){
-        $csvArray[$i]=mt_rand(0,1000);
+    $fileName = "test.csv";
+    $maxArray = mt_rand(50,100);
+    $csvArray = [];
+    for ($i = 0; $i < $maxArray; $i++){
+        $csvArray[$i] = mt_rand(0,1000);
     }
     file_put_contents($fileName, '');
 
-    $fp=fopen($fileName,'w');
+    $fp = fopen($fileName,'w');
         fputcsv($fp, $csvArray);
 
     fclose($fp);
 
-    $Result="TRUE";
+    $Result = "TRUE";
     Return $Result;
 }
 
 function summFromCSV($filename){
-    $tempArray=[];
-    $row=1;
-    if(($handle=fopen("test.csv","r"))!==FALSE){
-        while (($data=fgetcsv($handle, 1000, ",")) !==FALSE){
-            $num=count($data);
+    $tempArray = [];
+    $row = 1;
+    if(($handle = fopen("test.csv","r"))!==FALSE){
+        while (($data = fgetcsv($handle, 1000, ",")) !==FALSE){
+            $num = count($data);
             echo "<p>$num полей св строке $row:<br></p>\n";
             $row++;
-            for($c=0;$c<$num;$c++){
+            for($c = 0; $c < $num; $c++){
                 if(($data[$c] % 2) == 0){
                     $tempArray[] = $data[$c];
                     //echo $data[$c]."<br>";
@@ -41,7 +41,7 @@ function summFromCSV($filename){
         }
         fclose($handle);
     }
-    $summ=array_sum($tempArray);
+    $summ = array_sum($tempArray);
     return $summ;
 }
 
